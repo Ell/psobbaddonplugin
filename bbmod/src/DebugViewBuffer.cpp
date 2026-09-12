5e6e3c9bdd4aaf0130f35d874f065b23b48acf6c
ellg <ell@ell.dev>
2026-09-12 18:30:49 -0500

    Revert "Contain Windows debug-print exceptions within the native logger"
    This reverts commit cf0f0fe6e1b1ad301746bf40a88ee2fff7cff0c2.

--- bbmod/src/DebugViewBuffer.cpp
+++ bbmod/src/DebugViewBuffer.cpp
@@ -2,16 +2,6 @@
 
 #include "log.h"
 
-// OutputDebugString uses a Windows debug-print exception even without an
-// attached debugger. Keep that diagnostic exception inside the logger instead
-// of allowing it to reach LuaJIT's exception machinery during error recovery.
-static void debug_output(const char* text)
-{
-    __try { OutputDebugStringA(text); }
-    __except (GetExceptionCode() == DBG_PRINTEXCEPTION_C ?
-              EXCEPTION_EXECUTE_HANDLER : EXCEPTION_CONTINUE_SEARCH) {}
-}
-
 DebugViewBuffer::~DebugViewBuffer()
 {
     sync(); // can be avoided
@@ -19,7 +9,7 @@ DebugViewBuffer::~DebugViewBuffer()
 
 int DebugViewBuffer::sync()
 {
-    debug_output(str().c_str());
+    OutputDebugString(str().c_str());
     g_lualog.AddLog("%s", str().c_str());
     str("");
 
